7c34f6bd0cdb9e3a6282c97fb650773268f7728e
marinhow <mario.vaz.henriques@gmail.com>
2012-12-21 09:53:57 +0100

    Added Selenium Tests

---
+++ SeleniumTests/TestAboutMenu.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace SeleniumTests
+{
+    /// <summary>
+    /// Test About
+    /// </summary>
+    [TestClass]
+    public class TestAboutMenu
+    {
+
+        private List<IWebDriver> _drivers;
+
+        [TestFixtureSetUp]
+        public void FixtureSetup()
+        {
+            _drivers = Utils.GetAvailableWebDrivers();
+
+        }
+
+        [SetUp]
+        public void TestSetUp()
+        {
+
+
+        }
+
+        [TestFixtureTearDown]
+        public void FixtureTearDown()
+        {
+            foreach (var webDriver in _drivers)
+            {
+                Utils.LogOut(webDriver);
+                Utils.Destroy(webDriver);
+            }
+        }
+
+
+        [Test]
+        public void S12T1()
+        {
+            foreach (var webDriver in _drivers)
+            {
+                webDriver.Navigate().GoToUrl("http://localhost:8080/Home/About");
+
+                IWebElement aboutMenuOption = webDriver.FindElement(By.LinkText("About"));
+
+                aboutMenuOption.Click();
+
+            }
+        }
+    }
+}
---
+++ SeleniumTests/Utils.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+
+namespace SeleniumTests
+{
+    static class Utils
+    {
+
+        //Start WebBrowsers
+        public static List<IWebDriver> GetAvailableWebDrivers()
+        {
+
+            var drivers = new List<IWebDriver>();
+
+            IWebDriver _ChromeDriver = new ChromeDriver();
+            _ChromeDriver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 30));
+            drivers.Add(_ChromeDriver);
+
+            //IWebDriver _IEDriver = new InternetExplorerDriver();
+            //_IEDriver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 30));
+            //drivers.Add(_IEDriver);
+
+            //IWebDriver _FireFoxDriver = new FirefoxDriver();
+            //_FireFoxDriver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 30));
+            //drivers.Add(_FireFoxDriver);
+
+            //NOT WORKING - https://code.google.com/p/selenium/wiki/SafariDriver
+            //IWebDriver _SafariDriver = new SafariDriver();
+            //_SafariDriver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 30));
+
+            return drivers;
+        }
+
+
+
+        //Automate Login
+        public static void MockLogIn(IWebDriver driver, string fødselsnummer)
+        {
+            try
+            {
+             
+
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+
+        //LogOut
+        public static void LogOut(IWebDriver driver)
+        {
+            try
+            {
+
+
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+
+        //Clean and Destroy drivers and processes
+        public static void Destroy(IWebDriver driver)
+        {
+            try
+            {
+                driver.Close();
+                driver.Quit();
+
+                Process[] processes = Process.GetProcessesByName("IEDriverServer");
+                if (processes.Length > 0)
+                {
+                    KillProcessByName("iexplore");
+                    KillProcessByName("IEDriverServer");
+                }
+
+                processes = Process.GetProcessesByName("chromedriver");
+                if (processes.Length > 0)
+                {
+                    KillProcessByName("chrome");
+                    KillProcessByName("chromedriver");
+                }
+
+            }
+            catch (Exception) { }
+
+        }
+
+        private static void KillProcessByName(string processName)
+        {
+            foreach (Process process in Process.GetProcessesByName(processName))
+            {
+                process.Kill();
+            }
+        }
+
+    }
+}
